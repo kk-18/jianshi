d5570d249559c5349151173caa30fc3592101996
kk_18 <805866462@qq.com>
2019-08-05 17:49:21 +0800

    数据驱动ddt

--- main/main.py
+++ main/main.py
@@ -1,24 +1,28 @@
 import unittest
 import openpyxl
 import os
-import ddt
+from ddt import ddt,data
 import json
 from common.forreq import Requests
 from common.forexcel import *
 
-
+@ddt
 class test_main(unittest.TestCase):
+    # 获取文件路径
+    excel_path = os.path.join(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0],
+                              r'data\test_case_web.xlsx')
+    tt = DoExcel(excel_path).read_cases('web')
 
     def setUp(self):
         print("----------测试开始-----------")
-    def test_aaa(self):
-        #获取文件路径
-        excel_path=os.path.join(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0],
-                                           r'data\test_case_web.xlsx')
-        tt=DoExcel(excel_path).read_cases('web')
-        #数据转换， json.loads()用于将str类型的数据转成dict
-        print(tt[1])
 
+    @data(*tt)#装饰测试方法，拿到几个数据数据就执行几条用例
+    def test_aaa(self,case):
+
+        #数据转换， json.loads()用于将str类型的数据转成dict
+
+        data=json.loads(case.data)
+        print(data[1])
         '''
         res=Requests(method=tt['method'],url=tt['url'])
         print("状态码:{0}，响应结果:{1}".format(res.get_status_code(),res.get_json()))
